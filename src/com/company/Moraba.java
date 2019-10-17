decf4887793e4dfa89d217302106599192ca3c2d
davoodPur <sample@gmail.com>
2019-10-17 17:53:25 +0330

    moraba kamel shod

---
+++ src/com/company/Moraba.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Moraba {
+    private int tol,arz;
+
+    public Moraba(int tol, int arz) {
+        this.tol = tol;
+        this.arz = arz;
+    }
+
+    public int getTol() {
+        return tol;
+    }
+
+    public void setTol(int tol) {
+        this.tol = tol;
+    }
+
+    public int getArz() {
+        return arz;
+    }
+
+    public void setArz(int arz) {
+        this.arz = arz;
+    }
+}
