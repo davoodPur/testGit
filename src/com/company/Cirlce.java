3e1b035635244eaaa5c23640f9597393e88a495d
davoodPur <sample@gmail.com>
2019-10-17 17:43:15 +0330

    ezafe kardan emkanat jadid

--- src/com/company/Cirlce.java
+++ src/com/company/Cirlce.java
@@ -2,6 +2,31 @@ package com.company;
 
 public class Cirlce extends Shape{
     private float r;
+    private int x;
+    private int y;
+
+    public Cirlce(String name, float r, int x, int y) {
+        super(name);
+        this.r = r;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 
     public Cirlce(String name, float r) {
         super(name);
