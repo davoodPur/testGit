ebf94c1bffcf7a80bafd024e2094c62608c98ded
KhAdIvFaR <sskhadivfar@gmail.com>
2019-10-17 18:07:16 +0330

    moraba ertefa ezafe shod

--- src/com/company/Moraba.java
+++ src/com/company/Moraba.java
@@ -1,26 +1,32 @@
 package com.company;
 
 public class Moraba {
-    private int tols,arzs;
+    private int tols,arzs,ertefa;
 
-    public Moraba(int tol, int arz) {
-        this.tol = tol;
-        this.arz = arz;
+
+    public Moraba(int tol, int arz, int ertefa) {
+        this.tols = tol;
+        this.arzs = arz;
+        this.ertefa = ertefa;
     }
 
     public int getTol() {
-        return tol;
+        return tols;
     }
 
     public void setTol(int tol) {
-        this.tol = tol;
+        this.tols = tol;
     }
 
     public int getArz() {
-        return arz;
+        return arzs;
     }
 
     public void setArz(int arz) {
-        this.arz = arz;
+        this.arzs = arz;
     }
+
+    public int getErtefa() { return ertefa; }
+
+    public void setErtefa(int ertefa) { this.ertefa = ertefa; }
 }
